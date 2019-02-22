8b13788cb99e69f4f916c95d4f8dfefe6ba86766
Dimitar Todorov <Dimitar.Todorov@progress.com>
2019-02-22 19:23:47 +0200

    Add more tests

--- demo-angular/e2e/tests.e2e.ts
+++ demo-angular/e2e/tests.e2e.ts
@@ -127,13 +127,25 @@ describe("DateTimePicker", () => {
     });
 
     it("Should select date from de_De locale picker and verify format", async () => {
-        await scrollToElement(driver, "preferred locale: de_DE", Direction.down);
+        await scrollToElement(driver, "zeit wählen", Direction.down);
+        const deLocale = await driver.findElementByText("preferred locale: de_DE");
+        expect(deLocale).to.exist;
         const datePicker = await driver.findElementByText("datum auswählen", SearchOptions.contains);
         await datePicker.click();
         const date = await getPickerDate(driver);
-        const acceptBtn = await driver.findElementByText("BestÄtigen", SearchOptions.contains);
-        const rejectBtn = await driver.findElementByText("Stornieren", SearchOptions.contains);
-        const title = await driver.findElementByText("Datum auswählen", SearchOptions.contains);
+        let selector = isAndroid ? "android.widget.Button" : "Bestätigen";
+        let acceptBtn;
+        let rejectBtn;
+        if(isAndroid){
+            let buttons = await driver.findElementsByClassName(selector);
+            acceptBtn = buttons[7];
+            rejectBtn = buttons[6];
+        }
+        else{
+            acceptBtn = await driver.findElementByText(selector, SearchOptions.contains);
+            rejectBtn = await driver.findElementByText("Stornieren", SearchOptions.exact);
+        }
+        const title = await driver.findElementByText("Datum auswählen", SearchOptions.exact);
         expect(acceptBtn).to.exist;
         expect(rejectBtn).to.exist;
         expect(title).to.exist;
@@ -147,4 +159,108 @@ describe("DateTimePicker", () => {
         expect(dateString).to.exist;
     });
 
+    it("Should select time from de_DE locale picker and verify format", async () => {
+        const timePicker = await driver.findElementByText("zeit wählen", SearchOptions.contains);
+        await timePicker.click();
+        const time = await getPickerTime(driver, 12);
+        let acceptBtn;
+        let rejectBtn;
+        if(isAndroid){
+            let buttons = await driver.findElementsByClassName("android.widget.Button");
+            acceptBtn = buttons[6];
+            rejectBtn = buttons[5];
+        }
+        else{
+            acceptBtn = await driver.findElementByText("Bestätigen", SearchOptions.exact);
+            rejectBtn = await driver.findElementByText("Stornieren", SearchOptions.exact);
+        }
+        const title = await driver.findElementByText("Zeit wählen", SearchOptions.exact);
+        expect(acceptBtn).to.exist;
+        expect(rejectBtn).to.exist;
+        expect(title).to.exist;
+        await acceptBtn.click();
+        let timeString = time.substr(0, time.indexOf(" "));
+        timeString = timeString + " nachm.";
+        console.log(timeString);
+        const dateField = await driver.findElementByText(timeString);
+        expect(timeString).to.exist;
+    });
+
+    it("Should scroll to custom format and verify values", async () => {
+        await scrollToElement(driver, "binding", Direction.down);
+        const customFromatLabel = await driver.findElementByText("custom format");
+        expect(customFromatLabel).to.exist;
+        const customFormatDate = await driver.findElementByText("date: 24 February 2019", SearchOptions.exact);
+        expect(customFormatDate).to.exist;
+        const customFormatTime = await driver.findElementByText("time: 01:00", SearchOptions.exact);
+        expect(customFormatTime).to.exist;
+    });
+
+    it("Should scroll to binding example and verify picker and label values", async () => {
+        await scrollToElement(driver, "css applied", Direction.down);
+        const bindingLabel = await driver.findElementByText("binding", SearchOptions.exact);
+        expect(bindingLabel).to.exist;
+        let selector = isAndroid ? "android.widget.EditText" : "XCUIElementTypeTextField"
+        let fields = await driver.findElementsByClassName(selector);
+        let timeField;
+        let dateField;
+        if(isAndroid){
+            timeField = fields[2];
+            dateField = fields[1];
+        } 
+        else{
+            let index = fields.length - 3;
+            console.log("index: " + index);
+            timeField = fields[index];
+            dateField = fields[index - 1];
+        }
+        await dateField.click();
+        let date = await getPickerDate(driver);
+        await clickOkBtn(driver);
+        let dateTime = new Date(date);
+        let year = dateTime.getFullYear().toString();
+        let dateLabel = dateTime.toString().substring(0, dateTime.toString().indexOf(year) + 4);
+        console.log(dateLabel);
+        let bindingDate = await driver.findElementByText(dateLabel, SearchOptions.contains);
+    });
+
+    it("Should scroll to css styled DatePicker and verify picker style", async () => {
+        await scrollToElement(driver, "tap to select time", Direction.down);
+        const cssPicker = await driver.findElementByText("Feb 24, 2019", SearchOptions.exact);
+        await cssPicker.click();
+        await getPickerDate(driver);
+        await driver.compareScreen("cssDatePicker");
+        await clickOkBtn(driver);
+    });
+
+    it("Should scroll to css styled TimePicker and verify picker style", async () => {
+        const cssPicker = await driver.findElementByText("1:00 AM", SearchOptions.exact);
+        await cssPicker.click();
+        await getPickerTime(driver, 12);
+        await driver.compareScreen("cssTimePicker");
+        await clickOkBtn(driver);
+    });
+
+    it("Should tap button to select date and verify button text", async () => {
+        let dateButton = await driver.findElementByText("tap to select date", SearchOptions.contains);
+        await dateButton.click();
+        const date = await getPickerDate(driver);
+        await clickOkBtn(driver);
+        const dateString = new Date(date).toLocaleDateString('de-DE', {
+            year: 'numeric', 
+            month: '2-digit', 
+            day: 'numeric' 
+        });
+        const dateField = await driver.findElementByText(dateString);
+        expect(dateString).to.exist;
+    })
+
+    it("Should tap button to select time and verify button text", async () => {
+        let timeButton = await driver.findElementByText("tap to select time", SearchOptions.contains);
+        await timeButton.click();
+        const time = await getPickerTime(driver, 24);
+        await clickOkBtn(driver);
+        timeButton = await driver.findElementByText(time);
+        expect(timeButton).to.exist;
+    });
 });
